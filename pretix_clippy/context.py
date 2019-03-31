d6158b6a9bdc313802e7aa16127035bd4d273dc8
Martin Gross <gross@rami.io>
2019-03-31 19:32:07 +0200

    Make contextprocessor include all html_head signals and not just override them

--- pretix_clippy/context.py
+++ pretix_clippy/context.py
@@ -1,14 +1,20 @@
 from django.template.loader import get_template
+from pretix.control.signals import html_head
 
 
 def contextprocessor(request):
     ctx = {}
 
     if request.path.startswith('/control/'):
+        _html_head = []
         template = get_template('pretix_clippy/control_head.html')
 
-        ctx = {
-            'html_head': template.render({})
-        }
+        if hasattr(request, 'event') and request.user.is_authenticated:
+            for receiver, response in html_head.send(request.event, request=request):
+                _html_head.append(response)
+
+        _html_head.append(template.render({}))
+
+        ctx['html_head'] = "".join(_html_head)
 
     return ctx
